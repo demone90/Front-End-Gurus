0e3448fb1ddaf3660f498850b4a7ffd3846a3742
Giuseppe Aina <ga@plista.com>
2022-03-17 14:58:25 +0100

    Adding promises to console.time file #new

--- session1/consoleTime.js
+++ session1/consoleTime.js
@@ -8,4 +8,16 @@ for(let i = b; i < a; i++) {
 	// do something
 }
 
+// TRY IT OUT WITH ASYNC AWAIT TOO AND SEE HOW IT BEHAVES!
+let promise = new Promise(function(resolve, reject) {
+	setTimeout(function() {
+	resolve("resolving");
+	},5000);
+	// executor (the producing code, "singer")
+});
+
+promise.then(function() {
+	console.log("promise complete");
+});
+
 console.timeEnd("timer-1");
